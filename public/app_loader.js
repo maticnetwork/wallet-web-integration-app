661ddfec2543fdbf4616d8ec65a35ec774b203b9
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2022-03-10 15:19:58 +0530

    implement app loader

---
+++ public/app_loader.js
@@ -0,0 +1,40 @@
+function loadDemoApp() {
+    const srcList = {
+        js: []
+    }
+    return fetch('/asset-manifest.json').then(response => {
+        return response.json();
+    }).then(assets => {
+        // const assets = response.json();
+        console.log(assets);
+        const files = assets.files;
+        for (const file in files) {
+            const path = files[file];
+            const splitted = files[file].split('.');
+            const last = splitted.pop();
+            if (last === 'js') {
+                srcList.js.push(path);
+            }
+        }
+
+        const promises = srcList.js.map(src => {
+            return loadScript(src);
+        });
+        return Promise.all(promises);
+    })
+
+
+}
+
+window.loadDemoApp = loadDemoApp;
+
+function loadScript(src) {
+    debugger;
+    const script = document.createElement('script');
+    script.src = src;
+    document.body.appendChild(script);
+    return new Promise((res, rej) => {
+        script.onload = res;
+        script.onerror = rej;
+    });
+}
